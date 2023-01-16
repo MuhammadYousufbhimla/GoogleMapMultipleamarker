20d7bf34c443a57bee4ca49d0373558c3e46b167
Muhammad Yousuf <Muhammadyousufgemini@gmail.com>
2023-01-16 19:24:27 +0500

    googlemapmarker add screen

---
+++ lib/goolgemap.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+import 'package:flutter/material.dart';
+
+class googlemapmarker extends StatefulWidget {
+  const googlemapmarker({super.key});
+
+  @override
+  State<googlemapmarker> createState() => _googlemapmarkerState();
+}
+
+class _googlemapmarkerState extends State<googlemapmarker> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
